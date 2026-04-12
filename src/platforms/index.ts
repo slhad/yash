14f6a510b04d0b771cbfbd3d4cadfc058e8950d1
Sylvain Rivierre <sylvainrivierre@gmail.com>
2026-04-12 21:49:36 +0200

    chore(platforms): add index re-exports for providers

---
+++ src/platforms/index.ts
@@ -0,0 +1,6 @@
+// Re-export platform providers for convenient imports
+export { YouTubeProvider } from './youtube';
+export { TwitchProvider } from './twitch';
+export { KickProvider } from './kick';
+
+export * from './base';
